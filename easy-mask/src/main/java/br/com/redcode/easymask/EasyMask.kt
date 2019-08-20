1ae1be4d5c7f7a61ca38389877a871a2021f74c6
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-08-20 18:14:00 -0300

    + Updating kotlin and gradle + Always returning watcher

--- easy-mask/src/main/java/br/com/redcode/easymask/EasyMask.kt
+++ easy-mask/src/main/java/br/com/redcode/easymask/EasyMask.kt
@@ -16,45 +16,62 @@ fun EditText.handleCPFandCNPJ(): CPFCNPJTextWatcher {
     return watcher
 }
 
-fun EditText.handleCPF() = addTextChangedListener(
-        MascaraNumericaTextWatcher.Builder()
-                .paraMascara("###.###.###-##")
-                .comValidador(Validador.CPF)
-                .build()
-)
+fun EditText.handleCPF(): MascaraNumericaTextWatcher? {
+    val watcher = MascaraNumericaTextWatcher.Builder()
+        .paraMascara("###.###.###-##")
+        .comValidador(Validador.CPF)
+        .build()
 
-fun EditText.handleCNPJ() = addTextChangedListener(
-        MascaraNumericaTextWatcher.Builder()
-                .paraMascara("##.###.###/####-##")
-                .comValidador(Validador.CNPJ)
-                .build()
-)
+    addTextChangedListener(watcher)
 
-fun EditText.handleMoney(hasSymbol: Boolean = true) = addTextChangedListener(
-        ValorMonetarioWatcher.Builder()
-                .apply {
-                    if (hasSymbol) comSimboloReal()
-                }
-                .comMantemZerosAoLimpar()
-                .build()
-)
+    return watcher
+}
 
+fun EditText.handleCNPJ(): MascaraNumericaTextWatcher? {
+    val watcher = MascaraNumericaTextWatcher.Builder()
+        .paraMascara("##.###.###/####-##")
+        .comValidador(Validador.CNPJ)
+        .build()
 
-fun EditText.handlePhone() = addTextChangedListener(
-        MascaraNumericaTextWatcher.Builder()
-                .paraMascara("(##) # ####-####")
-                .comValidador(Validador.TELEFONE)
-                .build()
-)
+    addTextChangedListener(watcher)
+
+    return watcher
+}
+
+fun EditText.handleMoney(hasSymbol: Boolean = true): ValorMonetarioWatcher? {
+    val watcher = ValorMonetarioWatcher.Builder()
+        .apply {
+            if (hasSymbol) comSimboloReal()
+        }
+        .comMantemZerosAoLimpar()
+        .build()
+
+    addTextChangedListener(watcher)
+
+    return watcher
+}
+
+fun EditText.handlePhone(): MascaraNumericaTextWatcher? {
+    val watcher = MascaraNumericaTextWatcher.Builder()
+        .paraMascara("(##) # ####-####")
+        .comValidador(Validador.TELEFONE)
+        .build()
+
+    addTextChangedListener(watcher)
+
+    return watcher
+}
+
+fun EditText.addNumericMask(mask: String): MascaraNumericaTextWatcher? {
+    val watcher = MascaraNumericaTextWatcher.Builder()
+        .paraMascara(mask)
+        .build()
+
+    addTextChangedListener(watcher)
+
+    return watcher
+}
 
 fun EditText.handleExpirationDate() = addNumericMask("##/##")
 fun EditText.handleDate() = addNumericMask("##/##/####")
 fun EditText.handleCEP() = addNumericMask("#####-###")
-
-fun EditText.addNumericMask(mask : String) {
-    addTextChangedListener(
-            MascaraNumericaTextWatcher.Builder()
-                    .paraMascara(mask)
-                    .build()
-    )
-}
